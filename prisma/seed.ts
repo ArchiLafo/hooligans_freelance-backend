8f34cb87c3f166490850f67165f2c6323bd66e9b
Kirill Ruchin <slimeboy871@gmail.com>
2023-07-18 14:08:04 +0700

    Added seed comand

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,39 +1,70 @@
 import { PrismaClient } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-const roundsOfHashing = 10;
-
 async function main() {
-  // const passwordKirill = await bcrypt.hash('password-kirill', roundsOfHashing);
-  // const passwordSlava = await bcrypt.hash('password-slava', roundsOfHashing);
+  const copywriting = await prisma.category.upsert({
+    where: { title: 'Копирайтинг' },
+    update: {},
+    create: {
+      title: 'Копирайтинг',
+      description: 'Создание продающих текстов и контента. Уникальные и убедительные сообщения для продвижения продуктов и услуг.',
+    },
+  });
 
-  // const user1 = await prisma.user.upsert({
-  //   where: { email: 'kirill@google.com' },
-  //   update: {
-  //     password: passwordKirill,
-  //   },
-  //   create: {
-  //     email: 'kirill@google.com',
-  //     name: 'Кирилл Ручин',
-  //     password: passwordKirill,
-  //   },
-  // });
+  const webdesign = await prisma.category.upsert({
+    where: { title: 'Веб-дизайн' },
+    update: {},
+    create: {
+      title: 'Веб-дизайн',
+      description: 'Создание привлекательных и интуитивно понятных веб-сайтов с учетом пользовательского опыта и эстетики.',
+    },
+  });
 
-  // const user2 = await prisma.user.upsert({
-  //   where: { email: 'slava@google.com' },
-  //   update: {
-  //     password: passwordSlava,
-  //   },
-  //   create: {
-  //     email: 'slava@google.com',
-  //     name: 'Вячеслав Прошин',
-  //     password: passwordSlava,
-  //   },
-  // });
+  const translation = await prisma.category.upsert({
+    where: { title: 'Перевод' },
+    update: {},
+    create: {
+      title: 'Перевод',
+      description: 'Перевод текстов на разные языки с сохранением смысла и стиля оригинала.',
+    },
+  });
 
-  // console.log({ user1, user2 });
+  const illustrations = await prisma.category.upsert({
+    where: { title: 'Иллюстрации' },
+    update: {},
+    create: {
+      title: 'Иллюстрации',
+      description: 'Создание качественных и оригинальных иллюстраций для использования в различных медиа-проектах.',
+    },
+  });
+
+  const mobiledevelopment = await prisma.category.upsert({
+    where: { title: 'Мобильная разработка' },
+    update: {},
+    create: {
+      title: 'Мобильная разработка',
+      description: 'Разработка мобильных приложений для разных платформ, учитывая лучшие практики и современные технологии.',
+    },
+  });
+
+  const photo = await prisma.category.upsert({
+    where: { title: 'Фотография' },
+    update: {},
+    create: {
+      title: 'Фотография',
+      description: 'Создание профессиональных фотографий для разных целей, включая портреты, природу, события и продукты.',
+    },
+  });
+  
+  const other = await prisma.category.upsert({
+    where: { title: 'Другое' },
+    update: {},
+    create: {
+      title: 'Другое',
+      description: 'Дополнительные услуги, которые не попадают в другие категории, такие как видеомонтаж, анимация, консалтинг и т.д.',
+    },
+  });
 }
 
 main()
