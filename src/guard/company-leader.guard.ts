88b9709a3c9640de579eac462ee50f2330be25b4
Kirill Ruchin <slimeboy871@gmail.com>
2023-07-18 15:49:43 +0700

    Added company guard

---
+++ src/guard/company-leader.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { CompanyService } from "src/company/company.service"
+import { UsersService } from "src/users/users.service"
+
+// проверяем, является ли пользователь владельцем компании
+@Injectable()
+export default class CompanyLeaderGuard implements CanActivate {
+    constructor (private readonly usersService: UsersService, private readonly companyService: CompanyService) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const {user, params} = context.switchToHttp().getRequest();
+        try {
+            await this.companyService.getById(Number(params.id));
+        } catch (error) {
+            return false;
+        }
+
+        console.log(params)
+        console.log("user: " + user + "company: " + params)
+        if (!user || !params) {
+            return false;
+        }
+
+        const userId = user.id;
+        const companyId = Number(params.id);
+        const checkedUser = await this.usersService.getById(userId)
+        console.log("checkedUser: " + userId)
+        const checkedCompany = await this.companyService.getById(companyId)
+        console.log("checkedCompany: " + companyId)
+
+        // либо ты автор
+        return (checkedUser.id === checkedCompany.leaderId);    
+    }
+}
+    
